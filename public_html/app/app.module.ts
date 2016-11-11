5140dae43ec3fe66e6d8a3199064fee0c2bb8570
arthurgabitov <arthurgabitov@gmail.com>
2016-11-11 12:23:42 +0300

    Попытка добавить движение для элементов списка

--- public_html/app/app.module.ts
+++ public_html/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule}     from '@angular/forms';
 import { AppComponent }   from './app.component';
-import {DraggableModule} from 'ng2-draggable';
+import {DraggableModule} from 'ng2-draggable/src';
 @NgModule({
     imports:      [
         BrowserModule,
