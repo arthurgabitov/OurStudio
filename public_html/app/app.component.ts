db47bbd4cc0d5959fca2c9555e72ee0043e68cae
arthurgabitov <arthurgabitov@gmail.com>
2016-11-11 12:26:25 +0300

    Попытка добавить движение для элементов списка

--- public_html/app/app.component.ts
+++ public_html/app/app.component.ts
@@ -13,7 +13,7 @@ const HEROES: Hero[] = [
     { id: 7, name: 'Dynama' },
     { id: 8, name: 'Dr IQ' },
     { id: 9, name: 'Magma' },
-    { id: 10, name: 'Tornado1' }
+    { id: 10, name: 'Tornado' }
 ];
 
 @Component({
@@ -22,10 +22,9 @@ const HEROES: Hero[] = [
     '<h2>Список</h2>' +
     '<ul class="heroes">' +
     '<li *ngFor="let hero of heroes">' +
-    '<div [draggable]><span>{{hero.id}}</span> {{hero.name}}</div>' +
+    '<span class="badge">{{hero.id}}</span> {{hero.name}}' +
     '</li>' +
-    '</ul>',
-    directives: [Draggable],
+    '</ul>'
     styles:[`
       .selected {
         background-color: #CFD8DC !important;
@@ -46,6 +45,33 @@ const HEROES: Hero[] = [
         padding: .3em 0;
         height: 1.6em;
         border-radius: 4px;
+      }
+      .heroes li.selected:hover {
+        background-color: #BBD8DC !important;
+        color: white;
+      }
+      .heroes li:hover {
+        color: #607D8B;
+        background-color: #DDD;
+        left: .1em;
+      }
+      .heroes .text {
+        position: relative;
+        top: -3px;
+      }
+      .heroes .badge {
+        display: inline-block;
+        font-size: small;
+        color: white;
+        padding: 0.8em 0.7em 0 0.7em;
+        background-color: #607D8B;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -4px;
+        height: 1.8em;
+        margin-right: .8em;
+        border-radius: 4px 0 0 4px;
       }
     `]
 })
